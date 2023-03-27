6ce337db2814a862c9d8b84fd06340701fbba4e2
jsp8680 <97013156+jsp8680@users.noreply.github.com>
2023-03-27 10:38:14 -0600

    Added by:   <your name> Date:       <date> Server listening on port 3000

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const express = require('express');
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+    });
+
+    app.listen('localhost', (req, res) => {
+    console.log('Server started on port 3000');
+    });
